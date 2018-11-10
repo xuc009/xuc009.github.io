8ffceb0ba4a1e700055ab1217e795c79083ce202
Connie <xuc009@newschool.edu>
2018-11-10 15:40:46 -0500

    track7draft

---
+++ .history/track6/assets/js/app_20181106151953.js
@@ -0,0 +1,22 @@
+var counter = 1;
+
+$('.frown').click(function(){
+  //alert("Hello! I am an alert box!!");
+  var $newDiv = $("<div></div>");
+  $newDiv.attr("id", "newDiv" + counter++);
+  $(".body_div").append($newDiv);
+});
+
+$('.smile').click(function(){
+    //alert("Hello! I am an alert box!!");
+    var $newDivb = $("<div></div>");
+    $newDivb.attr("id", "newDivb" + counter++);
+    $(".body_div1").append($newDivb);
+  });
+
+  $('.clear').click(function() {
+    console.log ('run')
+    document.getElementByClassName("body_div").innerHTML = " ";
+    // $('.body_div').addClass('hide');
+    // $('.body_div1').addClass('hide');
+});
---
+++ .history/track6/assets/js/app_20181106152944.js
@@ -0,0 +1,26 @@
+var counterSmile = 1;
+var counterFrown = 1;
+
+
+$('.frown').click(function(){
+  //alert("Hello! I am an alert box!!");
+  var $newDiv = $("<div></div>");
+  $newDiv.attr("id", "newDiv" + counterSmile++);
+  $(".body_div").append($newDiv);
+});
+
+$('.smile').click(function(){
+    //alert("Hello! I am an alert box!!");
+    var $newDivb = $("<div></div>");
+    $newDivb.attr("id", "newDivb" + counterFrown++);
+    $(".body_div1").append($newDivb);
+  });
+
+  $('.clear').click(function() {
+    console.log ('run')
+    document.getElementById('body_div').innerHTML = " ";
+    document.getElementById('body_div1').innerHTML = " ";
+    counter = 1;
+    // $('.body_div').addClass('hide');
+    // $('.body_div1').addClass('hide');
+});
